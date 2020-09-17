509a6218c875f994878cc8eb0ecfa8bd88b28457
nehuffman13 <69683807+nehuffman13@users.noreply.github.com>
2020-09-17 15:58:52 -0500

    Update main.py
    Adds code.

---
+++ PyBank/main.py
@@ -0,0 +1,15 @@
+#import 
+import os
+import csv
+
+#enters variable definitions 
+months = []
+profit_loss = []
+
+#creates start 
+count_months = 0
+profit_loss_total = 0
+
+budget_data_csv_path = os.path.join("Resources", "budget_data.csv")
+
+#header row 
